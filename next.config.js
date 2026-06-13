0443468d052ea40022d4925f7006afac0b3bb7ea
nurzaman-now <imannj69@gmail.com>
2026-06-13 15:13:14 +0700

    build: update deployment command to use npm run export and simplify next.config.js configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
+  images: {
+    unoptimized: true,
+  },
 };
 
-if (typeof module !== 'undefined') {
-    module.exports = nextConfig;
-} else {
-    export default nextConfig;
-}
+module.exports = nextConfig;
